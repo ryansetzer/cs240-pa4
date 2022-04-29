a3ec81719308bd6c554a9450b02992d6c58b0c6f
Ryan Setzer <rsetzah@wozniak.local>
2022-04-29 01:18:38 -0400

    All but 0 and 1 byte works

--- src/JMUnzip.java
+++ src/JMUnzip.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.nio.file.Files;
 import java.util.HashMap;
 
 /**
@@ -13,9 +12,47 @@ public class JMUnzip {
   private static void Decode(String[] args) {
     try {
       // Read-File
-      File readFile = new File(args[0]);
-    } catch (Exception e) {
+      FileInputStream fis = new FileInputStream(args[0]);
+      ObjectInputStream ois = new ObjectInputStream(fis);
+      HuffmanSave huffmansave = (HuffmanSave) ois.readObject();
+      ois.close();
+      // Write-File
+      FileOutputStream writeFile = new FileOutputStream(args[1]);
 
+
+      // Getting Objects from HuffmanSave
+      BitSequence bitSequence = huffmansave.getEncoding();
+      // I do not know why I need to add this space but when I remove it, it
+      // dies
+      String sequenceString = bitSequence.toString() + " ";
+      HashMap<Byte, Integer> frequencies = huffmansave.getFrequencies();
+      HuffmanTree huffmantree = new HuffmanTree(frequencies);
+      // Mapping Bytes with their Encodings
+      HashMap<String, Byte> encodingsWithKeys = new HashMap<>();
+      for (Byte key : frequencies.keySet()) {
+        encodingsWithKeys.put(huffmantree.findEncoding(key), key);
+      }
+      String[] encodings =
+        encodingsWithKeys.keySet().toArray(new String[encodingsWithKeys.size()]);
+      for (int i = 0; i < sequenceString.length(); i++) {
+        for (int j = 0; j < encodings.length; j++) {
+          if (sequenceString.substring(0, i).equals(encodings[j])) {
+            byte encoding = encodingsWithKeys.get(sequenceString.substring(0,
+              i));
+            String blah = encodings[j];
+            String substring = sequenceString.substring(0, i);
+            writeFile.write(encodingsWithKeys.get(sequenceString.substring(0,
+              i)));
+            sequenceString = sequenceString.substring(i);
+            i = 0;
+            break;
+          }
+        }
+      }
+      // Add end of file
+      writeFile.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
   }
